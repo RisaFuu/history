0f193a54a36b734f379f42eff1f3b55341d473aa
RisaFuu <89806633+RisaFuu@users.noreply.github.com>
2022-01-28 13:14:19 -0800

    homework

---
+++ next/pages/[gallery]/[album]/nearby.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  color : orange;
+  background-color: #A9A9A9;
+  border: 3px solid yellow;
+
+`
+
+export default function Nearby() {
+  return(
+    <Container>
+      <title>Van Arts Class</title>
+        <h1>Hello World</h1>
+    </Container>
+    )
+}
+
